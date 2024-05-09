91db85464e9fcb735024209b9f78f42adf48bfdc
Jerry Wang <ziyanwang1105@gmail.com>
2024-05-09 09:10:59 -0700

    start working on the controller of posting

--- app/controllers/api/postings_controller.rb
+++ app/controllers/api/postings_controller.rb
@@ -1,2 +1,37 @@
 class Api::PostingsController < ApplicationController
+    wrap_parameters include: Posting.attribute_names + ['userId', 'jobPosting']
+    before_action :require_logged_in, only: [:create, :update, :destroy]
+
+    def index
+        @postings = Posting.all
+        render :index
+    end
+
+    def show
+        @posting = Posting.find_by(id: params[:id])
+        if @posting
+          render :show
+        else
+          render json: @posting.errors.full_messages, status: 404
+        end
+    end
+
+    def create
+
+    end
+
+    def update
+
+
+    end
+
+    def destroy
+
+    end
+
+    private
+
+    def posting_params
+        params.require(:posting).permit(:user_id, :title, :body, :job_posting)
+    end
 end
